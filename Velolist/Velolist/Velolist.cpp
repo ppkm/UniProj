f71aaf08eae7f19d839cc548606f39552e3c1129
Vladimir <ppkm.vk@gmail.com>
2020-04-13 11:34:38 +0300

    Velolist + BiVelolist

--- Velolist/Velolist/Velolist.cpp
+++ Velolist/Velolist/Velolist.cpp
@@ -4,279 +4,144 @@
 
 using namespace std;
 
+struct List {
+
+	int value;
+	List* next_element = nullptr;
+};
+
 class Velolist {
 
 public:
-	int value;
-	Velolist* next_elem = nullptr;
+	List* head = nullptr;
+	int size = 0;
 	
 	Velolist() {
+		head = new List();
 	}
 
-	Velolist(int start_value) {
-	    value = start_value;
-	}
+	List* ElementAt(int index) {
 
-	int Size() {
+		if (index < 0 || index >= size) {
 
-		Velolist* buffer = next_elem;
-		int size = 0;
-		while (buffer != nullptr && buffer != this) {
-			size++;
-			buffer = buffer->next_elem;
-		}
+			if (index == -1)
+				return head;
 
-		return size;
-	}
-
-	Velolist* Element_At(int index) {
-
-		if (index < -1 || index >= Size()) {
-			cout << "Velolist: Out of bounds exception\n";
+			cout << "ERROR: Index out of bounds.";
 			system("pause");
 			exit(0);
 		}
 
-		Velolist* buffer = this;
-		for (int i = 0; i <= index; i++)
-			buffer = buffer->next_elem;
+		List* buffer = head->next_element;
+
+		for (int i = 1; i <= index; i++)
+			buffer = buffer->next_element;
 
 		return buffer;
 	}
-	
-	Velolist* Front() {
-		return Element_At(0);
+
+	void InsertAt(int index, int new_value) {
+
+		if (index < 0 || index > size) {
+			cout << "ERROR: Index out of bounds.\n";
+			system("pause");
+			return;
+		}
+
+		List* new_element = new List();
+		new_element->value = new_value;
+
+		List* prev_element = (index == 0) ? head : ElementAt(index-1);
+		List* curr_element = prev_element->next_element;
+
+		prev_element->next_element = new_element;
+		new_element->next_element = curr_element;
+
+		size++;
 	}
 
-	Velolist* Back() {
-		return Element_At(Size() - 1);
-	}
-
-	void Insert_At(int index, int new_value) {
-
-		Velolist* new_element = new Velolist(new_value);
-
-		Velolist* prev_element = Element_At(index-1);
-		Velolist* curr_element = prev_element->next_elem;
-
-		prev_element->next_elem = new_element;
-		new_element->next_elem = curr_element;
-	}
-
-	void Append(int new_value) {
-
-		Insert_At(Size(), new_value);
-	}
-
-	void Remove_At(int index) {
+	void RemoveAt(int index) {
 		
-		Velolist* b = Element_At(index);
-		Element_At(index - 1)->next_elem = b->next_elem;
-		delete b;
-	}
+		if (index < 0 || index >= size) {
+			cout << "ERROR: Index out of bounds\n";
+			system("pause");
+			return;
+		}
 
-	void Remove_Last() {
-		Remove_At(Size() - 1);
+		List* b = ElementAt(index);
+		ElementAt(index - 1)->next_element = b->next_element;
+		delete b;
+		size--;
 	}
 
 	void Link(int index) {
 
-		Velolist* last_elem = Back();
-		Velolist* link_elem = Element_At(index);
+		if (index < 0 || index >= size) {
+			cout << "ERROR: Index out of bounds.";
+			system("pause");
+			return;
+		}
+
+		List* last_elem = ElementAt(size-1);
+		List* link_elem = ElementAt(index);
 
 		for (int i = 0; i < index; i++)
-			Remove_At(0);
+			RemoveAt(0);
 
-		last_elem->next_elem = this;
+		last_elem->next_element = head;
 	}
-
-	void Unlink() {
-
-		Velolist* last_elem = Back();
-		last_elem->next_elem = nullptr;
-	}
-
-	void PrintList() {
-
-		cout << "\n\nSize: " << Size() << endl;
-		Velolist* buffer = this;
-		for (int i = 0; i < Size(); i++) {
-			cout << "List[" << i << "]: " << (buffer->next_elem)->value << endl;
-			buffer = buffer->next_elem;
-		}
-
-		if (Back()->next_elem != nullptr) {
-			cout << "List is linked.\n[" << Back()->value << "]-->[" << next_elem->value << "]\n";
-		}
-	}
-};
-
-class BiVelolist {
-
-public:
-	int value;
-	BiVelolist* next_elem = nullptr;
-	BiVelolist* prev_elem = nullptr;
-
-	BiVelolist() {
-	}
-
-	BiVelolist(int start_value) {
-		value = start_value;
-	}
-
-	int Size() {
-
-		BiVelolist* buffer = next_elem;
-		int size = 0;
-		while (buffer != nullptr && buffer != this) {
-			size++;
-			buffer = buffer->next_elem;
-		}
-
-		return size;
-	}
-
-	BiVelolist* Element_At(int index) {
-
-		if (index < -1 || index >= Size()) {
-			cout << "BiVelolist: Out of bounds exception\n";
-			system("pause");
-			exit(0);
-		}
-
-		BiVelolist* buffer = this;
-		for (int i = 0; i <= index; i++)
-			buffer = buffer->next_elem;
-
-		return buffer;
-	}
-
-	BiVelolist* Front() {
-		return Element_At(0);
-	}
-
-	BiVelolist* Back() {
-		return Element_At(Size() - 1);
-	}
-
-	void Insert_At(int index, int new_value) {
-
-		BiVelolist* newElement = new BiVelolist(new_value);
-
-		BiVelolist* prevElement = Element_At(index - 1);
-		BiVelolist* currElement = prevElement->next_elem;
-
-		prevElement->next_elem = newElement;
-
-		newElement->prev_elem = prevElement;
-		newElement->next_elem = currElement;
-
-		if (currElement == nullptr) { return; }
-
-		currElement->prev_elem = newElement;
-	}
-
-	void Append(int new_value) {
-
-		Insert_At(Size(), new_value);
-	}
-
-	void Remove_At(int index) {
-
-		if (index < 0) {
-			cout << "BiVelolist: Out of bounds exception\n";
-			system("pause");
-			exit(0);
-		}
-
-		BiVelolist* b = Element_At(index);
-
-		BiVelolist* prevElement = b->prev_elem;
-		BiVelolist* nextElement = b->next_elem;
-
-		if(prevElement != nullptr)
-			prevElement->next_elem = nextElement;
-
-		if (nextElement != nullptr)
-			nextElement->prev_elem = prevElement;
-
-		delete b;
-	}
-
-	void Remove_Last() {
-		Remove_At(Size() - 1);
-	}
-	
-	void Link(int index) {
-
-		if (index < 0)
-		{
-			cout << "BiVelolist: Index out of bounds.\n";
-			system("pause");
-			exit(0);
-		}
-
-		BiVelolist* last_elem = Back();
-		BiVelolist* link_elem = Element_At(index);
-
-		for (int i = 0; i < index; i++)
-			Remove_At(0);
-
-		last_elem->next_elem = this;
-		prev_elem = last_elem;
-	}
-
-	/* 0-1-2-[3]-4-5-\   ->>>  [3]-4-5-0-1-2
-	   \-----------/
-	*/
 
 	void Unlink(int index) {
 
-		if (prev_elem == nullptr)
-		{
-			cout << "BiVelolist: List isn't linked.\n";
+		if (!ElementAt(size-1)->next_element) {
+			cout << "List isn't linked.\n";
 			system("pause");
-			exit(0);
+			return;
 		}
 
-		if (Size() == 1)
+		if (size == 1) {
+			head->next_element->next_element = nullptr;
 			return;
+		}
 
-		BiVelolist* newFirst = Element_At(index);
-		BiVelolist* newLast = newFirst->prev_elem;
+		List* newStartElement = ElementAt(index);
+		List* currentLastElement = ElementAt(size - 1);
 
-		BiVelolist* currLast = prev_elem;
+		ElementAt(index - 1)->next_element = nullptr;
 
-		currLast->next_elem = next_elem;
-		next_elem->prev_elem = currLast;
+		currentLastElement->next_element = head->next_element;
 
-		prev_elem = nullptr;
-		next_elem = newFirst;
-
-		newFirst->prev_elem = this;
-		newLast->next_elem = nullptr;
+		head->next_element = newStartElement;
 	}
-	
+
 	void PrintList() {
 
-		cout << "\n\nSize: " << Size() << endl;
-
-		BiVelolist* buffer = this;
-
-		for (int i = 0; i < Size(); i++) {
-			cout << "List[" << i << "]: " << (buffer->next_elem)->value << endl;
-			buffer = buffer->next_elem;
+		if (size == 0) {
+			cout << "List is empty.\n";
+			return;
 		}
 
-		if (prev_elem != nullptr) {
-			cout << "List is linked.\n[" << prev_elem->value << "]-n->[" << prev_elem->next_elem->next_elem->value << "]\n";
-			cout << "[" << next_elem->value << "] -p->[" << next_elem->prev_elem->prev_elem->value << "]\n";
+		List* buffer = head->next_element;
+
+		int index = 0;
+
+		while (buffer && buffer != head) {
+
+			cout << "[" << index << "] " << buffer->value << endl;
+			buffer = buffer->next_element;
+			index++;
+		}
+
+		if (ElementAt(size - 1)->next_element) {
+
+			cout << "\nList is linked.\n";
+			cout << "[" << ElementAt(size - 1)->value << "] --next--> [" << head->next_element->value << "].\n";
+		}
+		else {
+			cout << "\nList isn't linked.\n";
 		}
 	}
 };
-
-
 
 const int INIT_VELOLIST_SIZE = 10;
 
@@ -285,189 +150,150 @@ void init(Velolist* vl) {
 	srand(time(NULL));
 
 	for (int i = 0; i < INIT_VELOLIST_SIZE; i++)
-		vl->Append((rand() % 100 + 1));
+		vl->InsertAt(0,(rand() % 100 + 1));
 }
 
-void init(BiVelolist* bvl) {
+void TestInsertAt(Velolist* vl) {
 
-	srand(time(NULL));
-
-	for (int i = 0; i < INIT_VELOLIST_SIZE; i++)
-		bvl->Append((rand() % 10 + 1));
-}
-
-void TestInsertAt(Velolist& vl) {
-
-	int new_value;
-	int index;
+	int index, value;
 
 	cout << "Index: ";
 	cin >> index;
 
-	cout << "Value: ";
-	cin >> new_value;
-
-	vl.Insert_At(index, new_value);
-}
-
-void TestInsertAt(BiVelolist& bvl) {
-
-	int new_value;
-	int index;
-
-	cout << "Index: ";
-	cin >> index;
-
-	cout << "Value: ";
-	cin >> new_value;
-
-	bvl.Insert_At(index, new_value);
-}
-
-void TestAppend(Velolist& vl) {
-
-	int value;
 	cout << "Value: ";
 	cin >> value;
 
-	vl.Append(value);
+	vl->InsertAt(index, value);
 }
 
-void TestAppend(BiVelolist& bvl)
-{
-	int value;
-	cout << "Value: ";
-	cin >> value;
-
-	bvl.Append(value);
-}
-
-void TestRemoveAt(Velolist& vl) {
-
+void TestRemoveAt(Velolist* vl) {
 	int index;
 	cout << "Index: ";
 	cin >> index;
 
-	vl.Remove_At(index);
+	vl->RemoveAt(index);
 }
 
-void TestRemoveAt(BiVelolist& bvl)
-{
-	int index;
-	cout << "Index: ";
-	cin >> index;
+void TestChainInsert(Velolist* vl) {
+	int size = 0;
 
-	bvl.Remove_At(index);
+	while (size <= 0) {
+		cout << "\nSize of chain: ";
+		cin >> size;
+	}
+
+	int startIndex = -1;
+
+	while (startIndex < 0 || startIndex > vl->size) {
+
+		cout << "\nStartIndex: ";
+		cin >> startIndex;
+	}
+
+	for (int i = 0; i < size; i++) {
+		int value;
+		cin >> value;
+		vl->InsertAt(startIndex + i, value);
+	}
 }
 
-void TestLink(Velolist& vl) {
+void TestChainDelete(Velolist* vl) {
 
-	int index;
-	cout << "Index: ";
-	cin >> index;
+	int startIndex = -1;
+	int endIndex = -1;
 
-	vl.Link(index);
+	while (startIndex < 0 || startIndex >= vl->size) {
+		cout << "\nStartIndex: ";
+		cin >> startIndex;
+	}
+
+	while (endIndex < startIndex || endIndex >= vl->size) {
+		cout << "\n\EndIndex: ";
+		cin >> endIndex;
+	}
+
+	for (int i = 0; i <= endIndex - startIndex; i++) {
+		vl->RemoveAt(startIndex);
+	}
 }
 
-void TestLink(BiVelolist& bvl)
-{
-	int index;
-	cout << "Index: ";
-	cin >> index;
+void TestLink(Velolist* vl) {
 
-	bvl.Link(index);
+	int index = -1;
+
+	if (vl->size == 0) {
+		cout << "List is empty.\n";
+		system("pause");
+		return;
+	}
+
+	while (index < 0 || index >= vl->size) {
+
+		cout << "\nIndex: ";
+		cin >> index;
+	}
+
+	vl->Link(index);
 }
 
-void TestUnlink(BiVelolist& bvl)
-{
-	int index;
-	cout << "Index: ";
-	cin >> index;
+void TestUnlink(Velolist* vl) {
 
-	bvl.Unlink(index);
+	if (!vl->ElementAt(vl->size - 1)) {
+		cout << "List isn't linked.\n";
+		system("pause");
+		return;
+	}
+
+	int index = -1;
+
+	while (index < 0 || index >= vl->size) {
+
+		cout << "Index: ";
+		cin >> index;
+	}
+
+	vl->Unlink(index);
 }
 
 void PrintVelolistMenu(Velolist * vl)
 {
 	int test_mode = 0;
 	vl->PrintList();
-	cout << "\nAvailable tests: \n";
-	cout << "[1] - Insert_At(int index, int value)\n";
-	cout << "[2] - Append(int value)\n";
-	cout << "[3] - RemoveAt(int index)\n";
-	cout << "[4] - RemoveLast()\n";
-	cout << "[5] - Link(int index)\n";
-	cout << "[6] - Unlink()\n";
+	cout << "\nAvailable test: \n";
+	cout << "[1] - InsertAt\n";
+	cout << "[2] - RemoveAt\n";
+	cout << "[3] - ChainInsert\n";
+	cout << "[4] - ChainDelete\n";
+	cout << "[5] - TestLink\n";
+	cout << "[6] - TestUnlink\n";
+
 
 	cout << "\nEnter test number:";
 	cin >> test_mode;
 
 	switch (test_mode) {
 	case 1:
-		TestInsertAt(*vl);
+		TestInsertAt(vl);
 		break;
 
 	case 2:
-		TestAppend(*vl);
+		TestRemoveAt(vl);
 		break;
 
 	case 3:
-		TestRemoveAt(*vl);
+		TestChainInsert(vl);
 		break;
 
 	case 4:
-		vl->Remove_Last();
+		TestChainDelete(vl);
 		break;
 
 	case 5:
-		TestLink(*vl);
+		TestLink(vl);
 		break;
 
 	case 6:
-		vl->Unlink();
-		break;
-	}
-}
-
-void PrintBiVelolistMenu(BiVelolist * bvl)
-{
-	bvl->PrintList();
-	
-	cout << "\nAvailable tests: \n";
-	cout << "[1] - Insert_At(int index, int value)\n";
-	cout << "[2] - Append(int value)\n";
-	cout << "[3] - RemoveAt(int index)\n";
-	cout << "[4] - RemoveLast()\n";
-	cout << "[5] - Link(int index)\n";
-	cout << "[6] - Unlink()\n";
-
-	int test_mode = 0;
-	cout << "\nEnter test number:";
-	cin >> test_mode;
-
-	switch (test_mode) {
-	case 1:
-		TestInsertAt(*bvl);
-		break;
-
-	case 2:
-		TestAppend(*bvl);
-		break;
-
-	case 3:
-		TestRemoveAt(*bvl);
-		break;
-
-	case 4:
-		bvl->Remove_Last();
-		break;
-
-	case 5:
-		TestLink(*bvl);
-		break;
-
-	case 6:
-		TestUnlink(*bvl);
+		TestUnlink(vl);
 		break;
 	}
 }
@@ -475,15 +301,13 @@ void PrintBiVelolistMenu(BiVelolist * bvl)
 void main() {
 
 	Velolist vl;
-	BiVelolist bvl;
 
 	init(&vl);
-	init(&bvl);
 
 	while (true) {
 		system("cls");
 		cout << "\t\tTest Start\n\n";
 
-		PrintBiVelolistMenu(&bvl);
+		PrintVelolistMenu(&vl);
 	}
 }
